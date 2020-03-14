591a33f96edf494781243bcbe8d738940843b35b
Ambrose Chua <ambrose@chua.family>
2020-03-14 19:56:18 +0800

    Scan for Webcam during generation
    Also clean up files

--- ansible/event-fetch-generate.sh
+++ ansible/event-fetch-generate.sh
@@ -2,6 +2,5 @@
 
 set -e
 
-echo "Fetching event inventory"
-
+echo "Fetching event inventory from Ambrose's infrastructure"
 ssh saguaro /usr/local/bin/wireguard-negotiator dump -i wg1 | go run event-generate.go opentech > event
--- ansible/event-generate.go
+++ ansible/event-generate.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"net"
 )
 
 func main() {
@@ -21,24 +22,34 @@ func main() {
 
 	for s.Scan() {
 		host := s.Text()
-		if strings.HasPrefix(host, "#") {
-			// Is a comment
+		ip := net.ParseIP(host).To16()
+		if ip == nil {
+			// Not a valid IPv6, skip
 			continue
 		}
-		if strings.HasSuffix(host, ":0") {
-			// Is a "system" host
+		if !bytes.Equal(ip[8:14], []byte{0, 0, 0, 0, 0, 0}) {
+			// First subnet is the recorders subnet
+			// Other subnets are "system" hosts
 			continue
 		}
-		hostname, roomId, roomType, err := discover(host, user)
+		hostname, roomType, roomId, webcamName, err := discover(host, user)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "host %s discovery failed: %v\n", host, err)
 		}
-		fmt.Printf("%s ansible_host=%s ansible_user=%s room_id=%s room_type=%s\n", hostname, host, user, roomId, roomType)
+		fmt.Printf("%s ansible_host=%s ansible_user=%s room_type=%s room_id=%s webcam_name=%s\n", hostname, host, user, roomType, roomId, webcamName)
 	}
 }
 
-func discover(host, user string) (hostname, roomId, roomType string, err error) {
-	cmd := exec.Command("/usr/bin/ssh", "-l", user, host, "sh", "-c", "hostname; cat room_type; cat room_id; exit 0")
+const script = `
+hostname;
+(cat room_type || echo) | head -n 1;
+(cat room_id || echo) | head -n 1;
+(ls /dev/v4l/by-id/ | grep C920 | grep index0 || echo) | cut -d - -f 1,2 | head -n 1;
+(cat /sys/class/net/en*/address || echo) | head -n 1;
+`
+
+func discover(host, user string) (hostname, roomType, roomId, webcamName string, err error) {
+	cmd := exec.Command("/usr/bin/ssh", "-l", user, host, "sh", "-c", script)
 	fmt.Fprintf(os.Stderr, "command: %s\n", cmd)
 	var out bytes.Buffer
 	cmd.Stdout = &out
@@ -55,5 +66,7 @@ func discover(host, user string) (hostname, roomId, roomType string, err error) 
 	roomType = strings.TrimSpace(roomType)
 	roomId, _ = out.ReadString('\n')
 	roomId = strings.TrimSpace(roomId)
+	webcamName, _ = out.ReadString('\n')
+	webcamName = strings.TrimSpace(webcamName)
 	return
 }
