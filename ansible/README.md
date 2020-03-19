cdfb87762a05643802234016879a8e31cf089ffe
Ambrose Chua <ambrose@chua.family>
2020-03-19 18:39:04 +0800

    Deploy higher bitrate and fix stream on LLI WiFi

---
+++ ansible/README.md
@@ -0,0 +1,7 @@
+
+# WARNING: It's a mess in here
+
+We're working on ironing this out, in a more reliable way, for future events.
+
+<!-- vim: set conceallevel=2 et ts=2 sw=2: -->
+
--- ansible/event-fetch-generate.sh
+++ ansible/event-fetch-generate.sh
@@ -3,4 +3,4 @@
 set -e
 
 echo "Fetching event inventory from Ambrose's infrastructure"
-ssh -p 222 root@fd11:f055:a514::1 /usr/local/bin/wireguard-negotiator dump -i wg1 | go run event-generate.go opentech > event
+ssh -p 222 root@fd11:f055:a514::1 /usr/local/bin/wireguard-negotiator dump -i wg1 | go run event-generate.go opentech
